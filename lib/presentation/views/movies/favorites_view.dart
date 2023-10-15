dec4f62bc6cc308a7439f5345567b89ce703e4c8
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-10-15 14:20:48 +0200

    MasonryGridView

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -20,14 +21,6 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
 
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: favoritesMovies.length,
-        itemBuilder: (context, index) {
-          final movie = favoritesMovies[index];
-          return ListTile(title: Text(movie.title));
-        }
-      )
-    );
+    return MovieMasonry(movies: favoritesMovies);
   }
 }
